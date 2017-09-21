f257a2af7c66994c310487eab2c0dcb4b891c138
Urno <Urno@gmail.com>
2017-09-21 12:33:31 +0900

    URL2

--- 과제 3.py
+++ 과제 3.py
@@ -1,21 +1,35 @@
 #텍스트 속의 단어 개수를 확인하는 프로그램
-f=open('text2.txt', 'r')
-D={};N={};order=[]
-for line in f:
-    sentence=line.split()
-    for k in range(len(sentence)-1) :
-        if sentence[k] not in D: D[sentence[k]]=1
-        else : D[sentence[k]]+=1
-print(D)
-for x,n in D.items():
-    if n not in N:
-        N[n]=[]
-        N[n].append(x)
-    else : N[n].append(x)
-for k in N.keys():
-    order.append(k)
-order.sort(reverse=True)
-for m in order:
-    L=", ".join(N[m])
-    if len(N[m])==1 : print("Number of word, %s in text, is %d" % (L, m))
-    else : print("Number of words, %s in text, are %d" % (L, m))
+import datetime, time
+import urllib.request
+
+url = "https://namu.wiki/w/Lobotomy%20Corporation"
+
+f = urllib.request.urlopen(url)
+#data = f.read().decode('euc-kr')
+data = f.read().decode('utf-8')
+
+begin = data.find("한국의 인디 게임.")
+end = data.find("느끼지 못할 수 있다")
+end += len("느끼지 못할 수 있다")
+
+print("total=", len(data))
+print("begin=", begin)
+print(data[begin:begin+100])
+print(data[end-100:end])
+print("length=", end-begin)
+
+speech = data[begin:end]
+speech = speech.split()
+
+analyze = {}
+for word in speech:
+    analyze[word] = analyze.get(word, 0) + 1
+
+flist = sorted(analyze.items(), key=lambda kv: kv[1], reverse=True)
+print("number of words is ", len(flist))
+
+cnt = 0
+for k, v in flist:
+    print(k, v)
+    if cnt > 100: break
+    cnt += 1
